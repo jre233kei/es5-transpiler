2384111ff03a90d5f7030a9ff1e491ed79662483
EC2 Default User <jre233kei+github@gmail.com>
2021-07-10 10:49:47 +0000

    update

---
+++ src/target.js
@@ -0,0 +1,12 @@
+const hoge = 100;
+let fuga = 100;
+
+const foo = (arr) => arr.map((a) => a*a)
+
+console.log(`result is ${foo([1,2,3])}`)
+
+async function a() {
+    await new Promise(resolve => setTimeout(resolve, 3000))
+}
+
+a();
