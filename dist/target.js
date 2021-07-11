1945f8aaed06f72e79522614dd16156e2a77e9ac
EC2 Default User <jre233kei+github@gmail.com>
2021-07-11 11:58:44 +0000

    add: capability for async/await

--- dist/target.js
+++ dist/target.js
@@ -1,19 +1,17 @@
 "use strict";
 
+require("core-js/stable");
+
+require("regenerator-runtime/runtime");
+
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
+// ここまで削除厳禁
+// ↓ここから変更したいプログラムを書き込んでください
 var hoge = 100;
 var fuga = 100;
-
-var foo = function foo(arr) {
-  return arr.map(function (a) {
-    return a * a;
-  });
-};
-
-console.log("result is ".concat(foo([1, 2, 3])));
 
 function b() {
   return _b.apply(this, arguments);
@@ -41,3 +39,11 @@ function _b() {
 }
 
 b();
+
+var foo = function foo(arr) {
+  return arr.map(function (a) {
+    return a * a;
+  });
+};
+
+console.log("result is ".concat(foo([1, 2, 3])));
--- src/target.js
+++ src/target.js
@@ -1,12 +1,19 @@
+// 正しく動作させるため必要なライブラリ・削除厳禁！！
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+// ここまで削除厳禁
+
+// ↓ここから変更したいプログラムを書き込んでください
+
 const hoge = 100;
 let fuga = 100;
-
-const foo = (arr) => arr.map((a) => a*a)
-
-console.log(`result is ${foo([1,2,3])}`)
 
 async function b() {
     await new Promise(resolve => setTimeout(resolve, 3000))
 }
 
 b();
+
+const foo = (arr) => arr.map((a) => a*a)
+
+console.log(`result is ${foo([1,2,3])}`)
